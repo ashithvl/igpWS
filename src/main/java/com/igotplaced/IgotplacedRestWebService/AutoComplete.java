91a26caaf98ceded235fc17e23f4a29f0dedb68b
ashithvl <ashithvl@gmail.com>
2017-05-12 12:25:12 +0530

    colg AutoComplete Over

--- src/main/java/com/igotplaced/IgotplacedRestWebService/AutoComplete.java
+++ src/main/java/com/igotplaced/IgotplacedRestWebService/AutoComplete.java
@@ -1,5 +1,60 @@
 package com.igotplaced.IgotplacedRestWebService;
 
-public class AutoComplete {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.json.JSONArray;
+
+import utils.Constants;
+
+@Path("/autocompleteService")
+public class AutoComplete {
+	Connection con = null;
+
+	List<String> colgList = new ArrayList<>();
+	JSONArray companyJSONArray = null;
+	
+	@GET
+	@Path("/searchCollege")
+	@Produces(MediaType.TEXT_HTML)
+	public String searchCollege() {
+
+		try {
+
+			con = Constants.ConnectionOpen();
+
+			String sql = "select collegename from college";
+
+			PreparedStatement ps = con.prepareStatement(sql);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				
+				colgList.add(rs.getString("collegename"));
+				
+			}
+			String[] colg = colgList.toArray(new String[colgList.size()]);
+			
+			companyJSONArray = new JSONArray(Arrays.asList(colg));
+		
+			con.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return companyJSONArray.toString();
+	}
 }
--- src/main/java/com/igotplaced/IgotplacedRestWebService/SpinnerValues.java
+++ src/main/java/com/igotplaced/IgotplacedRestWebService/SpinnerValues.java
@@ -22,13 +22,7 @@ public class SpinnerValues {
 	public String getYearOfPassOut() {
 		String yearOfPassOutArray[] = { "--Select the Passout Year--", "2025", "2024", "2023", "2022", "2021", "2020",
 				"2019", "2018", "2017", "2016", "2015", "2014" };
-		
-		/*ArrayList<String> yearOfPassOutArrayList = new ArrayList<String>();
-		for (String arraylist : yearOfPassOutArray) {
-			yearOfPassOutArrayList.add(arraylist);
-		}
-		
-*/
+
 		JSONArray companyJSONArray = new JSONArray(Arrays.asList(yearOfPassOutArray));
 		
 		return companyJSONArray.toString();
